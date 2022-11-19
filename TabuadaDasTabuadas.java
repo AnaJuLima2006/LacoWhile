fda37570348e3eec8b881b6633a3c756444ad13b
anajuprogrammer06 <105178561+AnaJuLima2006@users.noreply.github.com>
2022-11-19 18:16:46 -0300

    Add files via upload

---
+++ TabuadaDasTabuadas.java
@@ -0,0 +1,23 @@
+public class TabuadaDasTabuadas { //Exercício 1
+
+	public static void main(String[] args) {
+		int  numero = 1, resultado; 
+        
+        
+        while (numero<=10){
+	    	System.out.println("");
+	    	System.out.println("Tabuada do número " + numero + ":");
+	    	System.out.println("");
+    	  for(int i= 1; i <= 10; i++) {
+              
+              resultado = i * numero;
+              System.out.println(numero + "  x  " + i + "  =  " + resultado);
+              
+              
+          } 
+          numero++;
+        }
+
+	}
+
+}
